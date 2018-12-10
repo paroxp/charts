aeec980d4a927830f4d7188b167209a6ab353589
Ciro S. Costa <cscosta@pivotal.io>
2018-12-10 08:42:25 -0500

    Initializes the orphan branch

---
+++ README.md
@@ -0,0 +1,11 @@
+# maintenance
+
+Keeps `merged` branch always up to date with the branches that represent Concourse contributions to upstream that either didn't get merge yet or can't be submitted because they belong to release candidates.
+
+## Using
+
+If you have push access:
+
+    ./run.sh
+
+For documentation around `run.sh`, checkout the comments in the script itself.
---
+++ run.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# run - runs the maintenance task of keeping a "merged"
+# branch up to date with a set of branches that represent
+# PRs to the official `helm/charts` repository but didn't
+# get in yet.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly UPSTREAM=https://github.com/helm/charts
+readonly MERGED_BRANCH=${MERGED:-"merged"}
+readonly BRANCHES=(
+	origin/concourse/tls-secrets
+	origin/concourse/worker-rebalancing
+	origin/concourse/ephemeral-workers
+	origin/concourse-prometheus-scrape-pods
+	origin/concourse/worker-probes
+)
+
+main() {
+	show_info
+	update_upstream_state
+	update_merged_branch
+}
+
+show_info() {
+	echo "Updating merged branch with PR branches changes
+MERGED_BRANCH:  $MERGED_BRANCH
+UPSTREAM:       $UPSTREAM
+BRANCHES:       ${BRANCHES[@]}
+"
+}
+
+update_upstream_state() {
+	git remote add upstream $UPSTREAM || true
+	git fetch --all
+}
+
+update_merged_branch() {
+	git checkout $MERGED_BRANCH
+	git reset --hard upstream/master
+
+	for branch in ${BRANCHES[@]}; do
+		git merge \
+			--no-edit \
+			--strategy recursive \
+			--strategy-option theirs \
+			$branch
+	done
+
+	_update_chart_version
+
+	git push origin $MERGED_BRANCH -f
+}
+
+_update_chart_version() {
+	local chart_file="./stable/concourse/Chart.yaml"
+	local tmp_chart_file=$(mktemp /tmp/Chart.yaml.XXXXXX)
+
+	cat $chart_file |
+		sed 's/^version.*/version: 1337.0.0/g' >$tmp_chart_file
+	mv $tmp_chart_file $chart_file
+
+	git add --all .
+	git commit -m "[maintenance] bumps concourse version"
+}
+
+main "$@"
